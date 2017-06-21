aa868ea26764a461c7a3a74c330b4654e40d5863
lxltmac <2449382248@qq.com>
2017-06-22 00:03:36 +0800

    初步框架搭建

--- src/main.js
+++ src/main.js
@@ -2,14 +2,31 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import VueRouter from 'vue-router'
+import goods from 'components/goods/goods.vue'
+import ratings from 'components/ratings/ratings.vue'
+import seller from 'components/seller/seller.vue'
 
-Vue.config.productionTip = false
+import 'common/stylus/index.styl'
 
-/* eslint-disable no-new */
-new Vue({
+Vue.use(VueRouter)
+const routes = [
+  {path: '/goods', component: goods},
+  {path: '/ratings', component: ratings},
+  {path: '/seller', component: seller}
+]
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+})
+
+/*eslint-disable*/
+const app = new Vue({
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
 })
+router.push('/goods')
+
+
