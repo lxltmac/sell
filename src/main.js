f2630814ae5e661ef1474c5c1a1c2e07c52f777d
lxltmac <2449382248@qq.com>
2017-06-22 23:56:43 +0800

    顶部开发

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import goods from 'components/goods/goods.vue'
 import ratings from 'components/ratings/ratings.vue'
 import seller from 'components/seller/seller.vue'
@@ -10,6 +11,8 @@ import seller from 'components/seller/seller.vue'
 import 'common/stylus/index.styl'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
+
 const routes = [
   {path: '/goods', component: goods},
   {path: '/ratings', component: ratings},
