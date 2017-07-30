4b4da78a477c5796d14e06e2d91093e4e621f91e
lxltmac <2449382248@qq.com>
2017-07-30 22:32:53 +0800

    购物车第二阶段动画完善

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="cartcontrol">
-    <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
-      <transition name="move">
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count>0" @click="decreaseCart">
         <span class="inner icon-remove_circle_outline"></span>
-      </transition>
-    </div>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count>0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addCart"></div>
   </div>
@@ -48,17 +48,22 @@
     .cart-decrease
       display: inline-block
       padding: 6px
-      &.move-transition
-        opacity: 1
-        transform: translate3D(0,0,0)
+      opacity: 1
+      transform: translate3d(0, 0, 0)
       .inner
+        display: inline-block
         line-height: 24px
         font-size: 24px
         color: rgb(0,160,220)
-       &.move-enter,&.move-leave
-         transition: all 0.4s linear
-         opacity: 1
-         transform: translate3D(24px,0,0)
+        transition: all 0.2s linear
+        transform: rotate(0)
+       &.move-enter-active,&.move-leave-active
+         transition: all 0.2s linear
+       &.move-enter, &.move-leave-active
+         opacity: 0
+         transform: translate3d(24px,0,0)
+         .inner
+           transform: rotate(180deg)
     .cart-count
       display: inline-block
       vertical-align: top
@@ -70,4 +75,8 @@
       color: rgb(147,153,159)
     .cart-add
       display: inline-block
+      padding: 6px
+      line-height: 24px
+      font-size: 24px
+      color: rgb(0,160,220)
 </style>
